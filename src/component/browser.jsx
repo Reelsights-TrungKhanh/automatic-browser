9c1b70b4ec2be59e2afc3dcbae369bacbdf63ef1
trungkhanh2709 <trungkhanh@reelsights.com>
2025-08-26 20:55:50 +0700

    deploy

--- src/component/browser.jsx
+++ src/component/browser.jsx
@@ -16,7 +16,7 @@ export default function App() {
     if (!url) return;
 
     try {
-      const res = await fetch("http://localhost:4000/run-tango", {
+      const res = await fetch("https://server-auto-tango.onrender.com/run-tango", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url, prompt }),
