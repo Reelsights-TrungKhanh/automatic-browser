54c3f3afb82c1522f34fc4d363c09eaf174e156c
trungkhanh2709 <trungkhanh@reelsights.com>
2025-08-27 02:58:24 +0700

    add build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Browser from './component/browser.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
